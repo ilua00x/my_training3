8036ea092dc59f7a18c31a1c575450e4889203fc
ilua00x <90355490+ilua00x@users.noreply.github.com>
2024-12-13 03:43:19 +0400

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,13 @@
+my_string = input()
+print(my_string.upper())                      # 3.1 Выведите строку my_string в верхнем регистре.
+print(len(my_string))                         # 2.2 Вывести количество символов введённого текста
+my_string = input()
+print(my_string.lower())                      # 3.2 Выведите строку my_string в нижнем регистре.
+my_string = input()
+print(my_string.replace(' ', '')) # 3.3 Измените строку my_string, удалив все пробелы.
+my_string = input()
+print(my_string[0])                           # 3.4 Выведите первый символ строки my_string.
+print(my_string[:0])      # или так
+my_string = input()
+print(my_string[-1])                          # 3.5 Выведите последний символ строки my_string.
+# 2.2 просили но я чёт туплю) в плане ( часть ли это задания ) но сделал)
